02c3f2987117d17dbcb28d95ee3671a7c18cffc3
Muhammad Tiyas Fachreza Akbar <tiyas.akbar@gmail.com>
2020-11-29 01:59:48 +0700

    pemisahan dropdown admindan user solved

--- src/components/DropDown.jsx
+++ src/components/DropDown.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import Pic from '../img/Polygon1.png'
 import Actions from '../Context/Actions'
+import { useHistory } from 'react-router-dom'
 
 function DropDown({ action, setClicked }) {
+
+    const history = useHistory();
 
     const payHandler = () => {
         //Open Pay Comp
@@ -14,18 +17,34 @@ function DropDown({ action, setClicked }) {
         action.LOGOUT()
         setClicked(prevState => (!prevState))
     }
+    const addMusicHandler = () => {
+        history.push("/addmusic");
+        setClicked(prevState => (!prevState));
+    }
+    const addArtistHandler = () => {
+        history.push("/addartist");
+        setClicked(prevState => (!prevState));
+    }
 
     return (
-        <div className="d-flex flex-column col text-center white dDown">
+        < div className="d-flex flex-column col text-center white dDown" >
             <img src={Pic} alt="" className="segitiga" />
-            <div onClick={payHandler} className="d-flex justify-content-center align-items-center DownItem pointer">
-                <span>Pay</span>
-            </div>
+            {(action.dataLogin[0].role === 2) ? (
+                <div onClick={payHandler} className="d-flex justify-content-center align-items-center DownItem pointer">
+                    <span>Pay</span>
+                </div>
+            ) : (<>
+                <div onClick={addMusicHandler} className="d-flex justify-content-center align-items-center DownItem pointer">
+                    <span>Add Music</span>
+                </div>
+                <div onClick={addArtistHandler} className="d-flex justify-content-center align-items-center DownItem pointer">
+                    <span>Add Artist</span>
+                </div></>)}
             <div className="dropdown-divider" />
             <div onClick={outHandler} className="d-flex justify-content-center align-items-center DownItem pointer">
                 <span>Logout</span>
             </div>
-        </div>
+        </div >
     )
 }
 
